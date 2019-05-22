40c8135bd58e32bbeebde2b90d59b630175d810a
topongo <topongo@localhost.localdomain>
2019-05-22 15:02:34 +0200

    Added exapmle file

---
+++ src/js/example.js
@@ -0,0 +1,23 @@
+//rinominare il file in base alla sezione che si sta modificando
+
+
+
+//inserire qui le scene secondo la sintassi:
+//  var scenes = {
+//      [id_scena_1]]:{
+//          "yaw":...,
+//          "pitch":...,
+//          ...
+//          ...
+//          ...
+//      },
+//      [id_scena_2]:{
+//        "yaw":...,
+//      "pitch":...,
+//      ...
+//      },
+//      ...
+//  }
+//
+
+var scenes = {};
